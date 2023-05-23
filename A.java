f166638f02f2d9dd6c83a320b7ab4f5e260ce6da
˗ˏˋShubha ´ˎ˗ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-05-22 20:40:42 -0400

    Add files via upload

---
+++ A.java
@@ -0,0 +1,20 @@
+class A{   //parent class
+	void money(){
+		System.out.println("1 million dollars"); 
+	}	
+}
+class B extends A{  //child class
+	void clothes(){
+		System.out.println("The child gets clothes");
+	}
+
+	public static void main(String[] args){
+		A a = new A();
+		a.money();  //this gets parent class property only
+		// this gets the properties of the parent and child class
+		System.out.println("*******************************");
+		B b = new B();
+		b.money();
+
+	}
+}
